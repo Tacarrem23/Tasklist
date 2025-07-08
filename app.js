4849b5c5ce12422e7ce1f8dc83acb3a799b9b4b0
Tacarrem23 <carremitchell729@gmail.com>
2025-07-07 20:25:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -21,3 +21,14 @@ app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send("Sorry! Something went wrong.");
 });
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.use('/users', require('./routes/users'));
+app.use('/tasks', require('./routes/tasks'));
+
+module.exports = app;
